4a592f403d455b326b2179e7d80081966410cf01
Shreyas <Shreyas@DESKTOP-T2COU3K.Home>
2021-11-27 20:31:50 +0000

    new commit

--- src/test/java/getRequest/GetData.java
+++ src/test/java/getRequest/GetData.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-// first commit
+// first commit message
 public class GetData {
 
 	@Test
